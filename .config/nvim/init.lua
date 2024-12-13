c47dd8ad2acfa3bd1ff0552d82eca3803d4f5be7
Kaamkiya <ziam3.1415@gmail.com>
2024-12-12 20:05:09 -0500

    feat: Switch Neovim config from VimScript to Lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,39 @@
+vim.go.compatible = false
+vim.go.lazyredraw = true
+vim.go.number = true
+vim.go.cursorline = true
+vim.go.cursorcolumn = true
+vim.go.cmdheight = 1
+vim.go.scrolloff = 4
+vim.go.wildmenu = true
+vim.go.swapfile = false
+vim.go.hlsearch = true
+vim.go.incsearch = true
+vim.go.magic = true
+
+vim.cmd.syntax "on"
+vim.cmd.filetype "on"
+vim.cmd.colorscheme "catppuccin_mocha"
+
+function contains(t, value)
+  for i = 1,#t do
+    if (t[i] == value) then
+      return true
+    end
+  end
+  return false
+end
+
+if (contains({"javascript", "javascriptreact", "html", "css", "typescript", "typescriptreact", "astro"}, vim.bo.filetype)) then
+    vim.bo.expandtab = true
+    vim.bo.tabstop = 2
+    vim.bo.shiftwidth = 2
+elseif (contains({"python", "rust", "d", "sh", "zig", "lua"}, vim.bo.filetype)) then
+    vim.bo.expandtab = true
+    vim.bo.tabstop = 4
+    vim.bo.shiftwidth = 4
+else
+    vim.bo.expandtab = false
+    vim.bo.tabstop = 8
+    vim.bo.shiftwidth = 8
+end
