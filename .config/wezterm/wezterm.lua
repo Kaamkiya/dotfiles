dc10228b163a121c516f99baa9db7d089fe8a143
Kaamkiya <ziam3.1415@gmail.com>
2024-12-18 18:58:16 -0500

    feat: Style wezterm tab bar

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,5 +8,42 @@ config.color_scheme = 'Catppuccin Mocha'
 
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+config.show_tab_index_in_tab_bar = false
+wezterm.on(
+    'format-tab-title',
+    function(tab, tabs, panes, config, hover, max_width)
+        local title = tab.tab_title
+        if #title < 1 then
+            title = tab.active_pane.title
+        end
+
+        title = wezterm.truncate_right(title, max_width)
+
+        local background = '#11111b'
+        local foreground = '#b4befe'
+        if tab.is_active then
+            background = '#b4befe'
+            foreground = '#11111b'
+        elseif hover then
+            background = '#1e1e2e'
+            foreground = '#b4befe'
+        end
+
+        local right_tri = ''
+        if tab.is_active then
+            right_tri = wezterm.nerdfonts.pl_left_hard_divider
+        end
+
+        return {
+            { Background = { Color = background } },
+            { Foreground = { Color = foreground } },
+            { Text = ' ' .. title .. ' ' },
+            { Background = { Color = '#11111b' } },
+            { Foreground = { Color = background } },
+            { Text = right_tri },
+        }
+    end
+)
 
 return config
