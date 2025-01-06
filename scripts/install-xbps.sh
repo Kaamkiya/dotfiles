cf104239d0928018e7aee323b69fd16ca477bf09
Kaamkiya <ziam3.1415@gmail.com>
2025-01-06 16:24:07 -0500

    feat: Add scripts to install basic packages

---
+++ scripts/install-xbps.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+pkgs=(
+  # Basic, everyday commands.
+  'git'
+  'wget'
+  'curl'
+  'xz'     # Void Linux is very minimal, so yes, you do have to install xz-utils yourself.
+  'zip'
+  'unzip'
+  'make'
+
+  # Languages
+  'gcc'      # C compiler
+  'go'       # Go compiler
+
+  # These are useful for GUI environments/computers with desktops.
+  'kitty'     # A terminal.
+  'firefox'   # A browser.
+
+  # Fun things, not necessarily useful.
+  'fastfetch'   # A quick system fetch.
+)
+
+YELLOW="\033[0;93m"
+RESET="\033[0m"
+
+if [ "$EUID" -ne 0 ]; then
+  echo -e "${YELLOW}Must be run as root. Exiting."
+  exit 1
+fi
+
+if hash xbps-install 2>/dev/null; then
+  echo "${YELLOW}xbps is not installed on your system. Exiting."
+  echo "${RESET}"
+  exit 1
+fi
+
+# Update and upgrade packages.
+xbps-install -Syu
+
+# Install packages.
+for package in ${pkgs[@]}; do
+  xbps-install $package
+done
