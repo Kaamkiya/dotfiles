2c494ba2d4f6ee5e0dc7df98197366d6b6d8d8db
Kaamkiya <ziam3.1415@gmail.com>
2026-08-26 12:37:35 -0400

    chore: Update dotfiles

--- .local/bin/lock.sh
+++ .local/bin/lock.sh
@@ -4,15 +4,32 @@ imgs=("handsoff.png" "itslocked.png")
 selected_img=${imgs[ $RANDOM % ${#imgs[@]} ]}
 echo $selected_img >> /tmp/SELECTEDBACKGROUND
 
-BLANK='#00000000'
-DEFAULT='#cdd6f4'
+# states: normal, clear, capslock, ver, wrong
+
+BLANK="#00000000"
 
 swaylock \
---inside-color=$BLANK      \
---ring-color=$DEFAULT      \
---line-color=$BLANK        \
---separator-color=$DEFAULT \
+--ring-color="#cdd6f4" \
+--line-color=$BLANK \
+--separator-color=$BLANK \
+--inside-color="#cdd6f466" \
+--ring-clear-color="#f9e2af" \
+--line-clear-color=$BLANK \
+--inside-clear-color="#f9e2af66" \
+--ring-caps-lock-color="#fab387" \
+--line-caps-lock-color=$BLANK \
+--inside-caps-lock-color="#fab38766" \
+--ring-ver-color="#74c7ec" \
+--line-ver-color=$BLANK \
+--inside-ver-color="#74c7ec66" \
+--ring-wrong-color="#f38ba8" \
+--line-wrong-color=$BLANK \
+--inside-wrong-color="#f38ba866" \
+\
+--font="JetBrainsMono Nerd Font" \
+--font-size=12 \
 \
 --image=$HOME/data/pictures/backgrounds/$selected_img \
 \
---show-failed-attempts
+--show-failed-attempts \
+--indicator-caps-lock
